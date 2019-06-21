2e72961990283030b2a059743a5053d33695e427
codeAl <codeal@junior.com>
2019-06-21 14:21:16 +0200

    bug(UI enhancement): fix carousel loading first image [Finishes#166835998]

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,31 +1,35 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    //do work
-});
-var slideIndex = -1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+window.onload = () => {
+    var slideIndex = 1;
+    // Next/previous controls
+    plusSlides = (n) => {
+        showSlides(slideIndex += n);
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+
+    // Thumbnail image controls
+    currentSlide = (n) => {
+        showSlides(slideIndex = n);
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+
+    showSlides = (n) => {
+        if (n != slideIndex) {
+            slideIndex = n;
+        }
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n <= 0) { slideIndex = slides.length }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        if (typeof slides[slideIndex - 1] != "undefined") {
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+    }
+
+    showSlides(slideIndex);
+
 }
