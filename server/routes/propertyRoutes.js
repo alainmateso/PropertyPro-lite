eb82c07abfb1097816dc26150ef8750c5fe59d0d
codeAl <codeal@junior.com>
2019-07-05 12:21:09 +0200

    -chore(API/endpoint): API endpoint for user to view a specific property [Finishes#167097278]

--- server/routes/propertyRoutes.js
+++ server/routes/propertyRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import PropertyController from '../controllers/propertyController';
 
 router.get('/properties', PropertyController.viewAllProperties);
+router.get('/property/:id', PropertyController.viewPropertyById)
 
 
 export default router;
