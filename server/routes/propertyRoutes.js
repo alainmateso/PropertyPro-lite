010c8f428692b6b1f2dfc2c479bb38df0b1da96c
codeAl <codeal@junior.com>
2019-07-07 20:59:31 +0200

    feature(API): API endpoint for agent to mark property as sold [Finishes#167097171]

--- server/routes/propertyRoutes.js
+++ server/routes/propertyRoutes.js
@@ -10,5 +10,6 @@ router.get('/property/:id', PropertyController.viewPropertyById);
 router.post('/property', multipartyMiddle, PropertyController.postNewProperty);
 router.delete('/property/:id', PropertyController.deleteProperty);
 router.patch('/property/:id', multipartyMiddle, PropertyController.updatePropertyDetails);
+router.patch('/property/:id/sold', PropertyController.markAsSold)
 
 export default router;
