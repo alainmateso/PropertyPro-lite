b746d9e8356bc5f4607293d7fd128ba907f9edc6
codeAl <codeal@junior.com>
2019-07-13 11:23:49 +0200

    chore(API): add API documentation [Starts#167098459]

--- server/index.js
+++ server/index.js
@@ -2,21 +2,23 @@
 /* eslint-disable indent */
 /* eslint-disable no-console */
 import express from 'express';
-
+import swaggerUI from 'swagger-ui-express';
+import documentation from '../apiDocumentation.json';
 
 import route from './routes/propertyRoutes';
 import userRouter from './routes/userRoutes';
 
 require('dotenv').config();
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
-
 app.use('/api/v1', route);
 app.use('/api/v1', userRouter);
+app.use('/documentation', swaggerUI.serve, swaggerUI.setup(documentation));
 
 app.listen(PORT, () => {
 	console.log(`server running on port ${PORT}...`);
